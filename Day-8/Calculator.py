c38e908ff9321231c209af01a5fbd23b201f2b79
Priyankar Shukla <165669263+priyankar1434@users.noreply.github.com>
2025-01-03 00:14:22 +0530

    Update Calculator.py

--- Day-8/Calculator.py
+++ Day-8/Calculator.py
@@ -1,1 +1,11 @@
+print("**********Calculator**********")
+m = 5
+n = 2
+print("For Addition", m+n)
+print("For Multiplication", m*n)
+print("For Division" ,m/n)
+print("For Floor Division" , m//n)
+print("For Modulus",m**n) 
+print("For Exponential :",m%n)
+
 
