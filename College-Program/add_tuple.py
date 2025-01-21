43ac15b23557c88c5e06c01e2b42bc9f74fa1ef9
Priyankar Shukla <165669263+priyankar1434@users.noreply.github.com>
2025-01-21 23:52:10 +0530

    Update add_tuple.py

--- College-Program/add_tuple.py
+++ College-Program/add_tuple.py
@@ -1,1 +1,22 @@
+l1=[]
+x=int(input("How Many Elements: "))
+i=0
+while(i<x):
+    y=int(input("Enter Your Elements"))
+    l1.append(y)
+    i+=1
+print(tuple(l1))
+
+l2=[]
+i=0
+while(i<x):
+    b=int(input("Enter Your Elements:"))
+    l2.append(b)
+    i+=1
+l3=[]
+i=0
+while(i<x):
+    l3.append(l1[i]+l2[i])
+    i+=1
+print(tuple(l3))
 
