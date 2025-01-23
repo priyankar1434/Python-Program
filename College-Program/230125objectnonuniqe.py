62d0c061aa4dd605084260da381748bed5d708c3
Priyankar Shukla <165669263+priyankar1434@users.noreply.github.com>
2025-01-23 19:44:56 +0530

    Update 230125objectnonuniqe.py

--- College-Program/230125objectnonuniqe.py
+++ College-Program/230125objectnonuniqe.py
@@ -1,1 +1,20 @@
+s1=set()
+x=int(input("How many elements in 1st Object: "))
+i=0
+while(i<x):
+    s1.add(int(input("Enter Your Elements: ")))
+    i+=1
+print(s1)
+s2=set()
+y=int(input("How many elements in 2nd Object: "))
+i=0
+while(i<y):
+    s2.add(int(input("Enter Your Elements:")))
+    i+=1
+print(s2)
+s3=s2.union(s1)
+s4=s2.intersection(s1)
+s5=s3-s4
+print("Non Uniqe Elements are: ",s5)
+
 
