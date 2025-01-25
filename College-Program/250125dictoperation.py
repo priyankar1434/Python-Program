34454cd78ec5e90121deccad5fb4bfcf56bbc861
Priyankar Shukla <165669263+priyankar1434@users.noreply.github.com>
2025-01-25 13:50:10 +0530

    Update 250125dictoperation.py

--- College-Program/250125dictoperation.py
+++ College-Program/250125dictoperation.py
@@ -1,1 +1,20 @@
+d1={}
+x=int(input("How Many Dict Objects:"))
+i=0
+while(i<x):
+    d2=int(input("Enter Key:"))
+    d3=input("Enter Data:")
+    d1[d2]=d3
+    i+=1
+print(type(d1))
+s=d1.keys()
+for p in s:
+    print("Keys are:",p)
+o=d1.values()
+for q in o:
+    print("Data Values are:",q)
+l=d1.items()
+for r in l:
+    print("Items are:",r)
+
 
