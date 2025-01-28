493b6be782ba5462289f781da8ad313ddb9df37a
Priyankar Shukla <165669263+priyankar1434@users.noreply.github.com>
2025-01-28 16:59:42 +0530

    Update 280125countoddeven.py

--- College-Program/280125countoddeven.py
+++ College-Program/280125countoddeven.py
@@ -1,1 +1,20 @@
+l1=[]
+countodd=0
+counteven=0
+
+n=int(input("Enter Elements:"))
+i=0
+while(i<n):
+    y=int(input("Enter Elements"))
+    l1.append(y)
+    i+=1
+l2=tuple(l1)
+print(l2)
+for x in l2:
+    if (x%2==0):
+        counteven+=1
+    else:
+        countodd+=1
+print("Even:",counteven)
+print("Odd:",countodd)
 
