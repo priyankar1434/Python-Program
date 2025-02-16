4332fe45afbb4e468c2afc73731864999d0a62ab
Priyankar Shukla <165669263+priyankar1434@users.noreply.github.com>
2025-02-17 00:41:45 +0530

    Update String-operation.py

--- Day-13/String-operation.py
+++ Day-13/String-operation.py
@@ -1,1 +1,24 @@
+city = "!allahabad!"
+print('1:',city.lower())
+print('2:',city.strip())
+print('3:',city.rstrip())
+print('4:',city.replace("!","@"))
+print('5:',city.split())
+print('6:',city.capitalize())
+print('7:',city.center(10))
+print('8:',city.count("a"))
+print('9:',city.endswith("!"))
+print('10:',city.find("!"))
+print('11:',city.index("!"))
+print('12:',city.isalnum())
+print('13:',city.isalpha())
+print('14:',city.islower())
+print('15:',city.isprintable())
+print('16:',city.isspace())
+print('17:',city.istitle())
+print('18:',city.isupper())
+print('19:',city.startswith('!'))
+print('20:',city.swapcase())
+
+
 
