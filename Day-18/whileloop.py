a866a07d1bb66a365041dd89eb02c8916d16d392
Priyankar Shukla <165669263+priyankar1434@users.noreply.github.com>
2025-03-01 23:39:10 +0530

    Add files via upload

---
+++ Day-18/whileloop.py
@@ -0,0 +1,18 @@
+print("While Loop")
+i=0
+while(i<=10):
+    print("Number is:",i)
+    i+=1
+print("Iteration")
+i=int(input("Your Chouce:"))
+while(i<=38):
+    print(i)
+    i+=1
+print("Reverse Order")
+count=5
+while(count>0):
+    print(count)
+    count=count-1
+else:
+    print("I am inside else")
+print("Do While Looooooop")
