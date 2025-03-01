970cfee94f1493b9f7c503964bdad5a385554d71
Priyankar Shukla <165669263+priyankar1434@users.noreply.github.com>
2025-03-01 23:38:36 +0530

    Add files via upload

---
+++ Day-17/forloop.py
@@ -0,0 +1,14 @@
+#simple for loop:
+name=input("Enter Name:")
+for i in name:
+    print(i)
+#list looop
+colour=[]
+p=0
+n=int(input("How many number of list:"))
+while(p<n):
+    l1=input()
+    colour.append(l1)
+    p+=1
+print("list is:",colour)
+    
